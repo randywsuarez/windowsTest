0240d9770554ef7cf35715844eb541b62b786b9d
Randy Suarez <suarez9ster@gmail.com>
2024-02-05 16:32:45 -0600

    Cambio de formato a base64 txt

--- src/boot/txt.js
+++ src/boot/txt.js
@@ -17,22 +17,49 @@ module.exports = ({ Vue }) => {
 			}
 		}
 
-		// Ruta completa del archivo de texto
+		// Ruta completa del archivo de texto original
 		const textFilePath = path.join(logsFolderPath, fileName + '.txt')
 
-		// Guarda el contenido del archivo de texto
+		// Guarda el contenido del archivo de texto original
 		try {
 			fs.writeFileSync(textFilePath, fileContent)
-			console.log(`Archivo de texto guardado: ${textFilePath}`)
+			console.log(`Archivo de texto original guardado: ${textFilePath}`)
 			sessionStorage.setItem('txt', textFilePath)
 		} catch (error) {
-			console.error('Error al guardar el archivo de texto:', error)
+			console.error('Error al guardar el archivo de texto original:', error)
 		}
 
-		// Agrega el archivo de texto al prototipo "textFile"
+		// Ruta completa del archivo de texto en Base64
+		const base64FilePath = path.join(logsFolderPath, fileName + '.txt')
+
+		// Guarda el contenido del archivo de texto en Base64
+		const base64Content = Buffer.from(fileContent).toString('base64')
+
+		try {
+			fs.writeFileSync(base64FilePath, base64Content)
+			console.log(`Archivo de texto en Base64 guardado: ${base64FilePath}`)
+			sessionStorage.setItem('base64Txt', base64FilePath)
+		} catch (error) {
+			console.error('Error al guardar el archivo de texto en Base64:', error)
+		}
+
+		// Agrega información sobre los archivos al prototipo "textFile"
 		Vue.prototype.$textFile = {
-			name: fileName,
-			path: textFilePath,
+			original: {
+				name: fileName,
+				path: textFilePath,
+			},
+			base64: {
+				name: fileName + '_base64',
+				path: base64FilePath,
+			},
+		}
+		return {
+			SerialNumber: fileName,
+			EmployeeID: '',
+			FileType: '2',
+			fileExtension: '.txt',
+			fileBase64Str: base64FilePath,
 		}
 	}
 }
